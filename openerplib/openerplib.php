084b349003d4e2bbe91db2f74eb342e3f6be4806
Benito Rodríguez Arcos <brarcos@gmail.com>
2012-03-20 16:15:37 +0100

    Update openerplib/openerplib.php

--- openerplib/openerplib.php
+++ openerplib/openerplib.php
@@ -1,4 +1,13 @@
 <?php
+	/*
+	 * This file is part of the openerplib.
+	 *
+	 * (c) Benito Rodriguez <brarcos@gmail.com>
+	 *
+	 * For the full copyright and license information, please view the LICENSE
+	 * file that was distributed with this source code.
+	 */
+	 
 	require_once(dirname(__FILE__) . '/xmlrpc.inc');
 	require_once(dirname(__FILE__) . '/openerplib.inc.php');
 
