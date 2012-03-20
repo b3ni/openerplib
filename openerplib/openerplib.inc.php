1749700f15298b52c4d9634a0cc216705a1a75b4
Benito Rodríguez Arcos <brarcos@gmail.com>
2012-03-20 16:18:43 +0100

    Update openerplib/openerplib.inc.php

--- openerplib/openerplib.inc.php
+++ openerplib/openerplib.inc.php
@@ -1,8 +1,17 @@
 <?php
 
-define('_OPENERPLIB_BD_', '');
-define('_OPENERPLIB_UID_', 0);
-define('_OPENERPLIB_PASSWD_', '');
-define('_OPENERPLIB_URL_', 'http://<URL>/xmlrpc');
+  /*
+   * This file is part of the openerplib.
+	 *
+	 * (c) Benito Rodriguez <brarcos@gmail.com>
+	 *
+	 * For the full copyright and license information, please view the LICENSE
+	 * file that was distributed with this source code.
+	 */
+
+  define('_OPENERPLIB_BD_', '');
+  define('_OPENERPLIB_UID_', 0);
+  define('_OPENERPLIB_PASSWD_', '');
+  define('_OPENERPLIB_URL_', 'http://<URL>/xmlrpc');
 
 ?>
